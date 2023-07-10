1487519bc19c9d4041ffb0ffd305f4be1180602c
Damaris <kushkangogo09@gmail.com>
2023-07-10 16:02:50 +0200

    EntityBase and StubMiddleware modification

--- smart-contract/hyperledger-fabric/v2/java/src/main/java/hu/bme/mit/ftsrg/tpcc/entities/EntityInterface.java
+++ smart-contract/hyperledger-fabric/v2/java/src/main/java/hu/bme/mit/ftsrg/tpcc/entities/EntityInterface.java
@@ -1,10 +1,14 @@
 package hu.bme.mit.ftsrg.tpcc.entities;
 
-public interface EntityInterface {
+public interface EntityInterface<AssetType> {
     String getType();
     String[] getKeyParts();
     byte[] toBuffer();
     void fromBuffer(byte[] buffer);
     String toJson();
     void fromJson(String json);
+    //void fromObject(AssetType obj);
 }
+
+
+ 
