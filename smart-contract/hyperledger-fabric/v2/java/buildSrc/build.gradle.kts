8446e826a84420a0e1f7544dccf22f76937c8088
bp99 <bertalan.peter@bp99.eu>
2024-05-27 10:24:38 +0200

    Improve TPC-C build scripts

--- smart-contract/hyperledger-fabric/v2/java/buildSrc/build.gradle.kts
+++ smart-contract/hyperledger-fabric/v2/java/buildSrc/build.gradle.kts
@@ -11,5 +11,17 @@ dependencies {
   implementation("net.lingala.zip4j:zip4j:2.11.5")
 }
 
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
+}
+
+kotlin {
+  jvmToolchain {
+    languageVersion = JavaLanguageVersion.of(17)
+  }
+}
+
 group = "hu.bme.mit.ftsrg.gradle"
 version = "0.1.0"
--- smart-contract/hyperledger-fabric/v2/java/buildSrc/settings.gradle.kts
+++ smart-contract/hyperledger-fabric/v2/java/buildSrc/settings.gradle.kts
@@ -1,6 +1,10 @@
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
